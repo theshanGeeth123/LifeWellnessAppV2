93732ea801588d476a10173e34363b96f3d5ca7d
Theshan <theshangeethanjana@gmail.com>
2025-10-17 16:26:12 +0530

    Landing pages integerated

---
+++ android-test/app/src/main/java/com/example/assignment3/MainActivity5.kt
@@ -0,0 +1,25 @@
+package com.example.assignment3
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity5 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main5)
+
+        val btnNext = findViewById<Button>(R.id.button2)
+
+        btnNext.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
